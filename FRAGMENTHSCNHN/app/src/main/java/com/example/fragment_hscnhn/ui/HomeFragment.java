2eb92e309826278ed8589541b5027da605de5b33
vvhoang2411 <vvh1232004@gmail.com>
2025-08-16 08:55:57 +0700

    demo3

---
+++ FRAGMENTHSCNHN/app/src/main/java/com/example/fragment_hscnhn/ui/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.example.fragment_hscnhn.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.fragment_hscnhn.R;
+
+public class HomeFragment extends Fragment {
+    EditText etName;
+    Button btnGoProfile;
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
